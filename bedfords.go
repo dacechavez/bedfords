f7b3858b0d5fde1667a78380d5710ee84838575e
dacechavez <dacechavez@gmail.com>
2015-07-15 21:08:30 +0200

    working example with raw html

--- bedfords.go
+++ bedfords.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
 func main() {
-	
+	url := "http://www.dn.se"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Errorf("%v\n", err)
+	}
+
+	if resp.StatusCode != 200 {
+		fmt.Errorf("HTTP Response Error %d\n", resp.StatusCode)
+	}
+
+	// Close the response once we return from the function.
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 }
