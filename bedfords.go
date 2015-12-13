c386e0767d58c5c147c947b81c44fc459954eccf
dacechavez <dacechavez@gmail.com>
2015-12-13 01:44:39 +0100

    new population data

--- bedfords.go
+++ bedfords.go
@@ -1,62 +1,60 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
-	"golang.org/x/net/html"
-	"net/http"
+	"io/ioutil"
+	"os"
 )
 
+type Root struct {
+	XMLName xml.Name `xml:"Root"`
+	Data
+}
+
+type Data struct {
+	XMLName xml.Name `xml:"data"`
+	Records []Record
+}
+
+type Record struct {
+	XMLName xml.Name `xml:"record"`
+	Fields  []Field
+}
+
+type Field struct {
+	XMLName xml.Name `xml:"field"`
+	Value   int      `xml:"value"`
+	Name    string   `xml:"name,attr"`
+}
+
 func main() {
-	url := "https://sports.yahoo.com/nfl/teams/den/stats"
-	resp, err := http.Get(url)
+	xmlFile, err := os.Open("data.xml")
 	if err != nil {
-		fmt.Errorf("%v\n", err)
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer xmlFile.Close()
+
+	bytes, _ := ioutil.ReadAll(xmlFile)
+
+	var q Root
+	xml.Unmarshal(bytes, &q)
+
+	fmt.Println(q)
+
+	field := &Field{Value: 2, Name: "hello"}
+	fields := []Field{*field}
+	record := &Record{Fields: fields}
+	records := []Record{*record}
+	data := &Data{Records: records}
+	root := &Root{Data: *data}
+
+	output, err := xml.MarshalIndent(root, "", "    ")
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
 	}
 
-	if resp.StatusCode != 200 {
-		fmt.Errorf("HTTP Response Error %d\n", resp.StatusCode)
-	}
-
-	d := html.NewTokenizer(resp.Body)
-	// for {
-	// 	tt := z.Next()
-	// 	if tt == html.ErrorToken {
-	// 		fmt.Println(" error on token")
-	// 	}
-
-	// }
-	for {
-		// token type
-		tokenType := d.Next()
-		if tokenType == html.ErrorToken {
-			return
-		}
-		token := d.Token()
-		switch tokenType {
-		case html.StartTagToken: // <tag>
-			// type Token struct {
-			//     Type     TokenType
-			//     DataAtom atom.Atom
-			//     Data     string
-			//     Attr     []Attribute
-			// }
-			//
-			// type Attribute struct {
-			//     Namespace, Key, Val string
-			// }
-			if token.DataAtom == 0x4702 { // <p> tag
-				tt := d.Next()
-				if tt == html.TextToken {
-					fmt.Println(d.Token())
-				}
-			}
-		case html.TextToken: // text between start and end tag
-		case html.EndTagToken: // </tag>
-		case html.SelfClosingTagToken: // <tag/>
-
-		}
-	}
-	defer resp.Body.Close()
-	//body, err := ioutil.ReadAll(resp.Body)
-	//fmt.Println(string(body))
+	fmt.Println(string(output))
 }
